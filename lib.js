8a3b4171de5ab1a0d39d3d7c074d60cac752f7ae
Mark Tyneway <mark.tyneway@gmail.com>
2020-09-29 10:20:15 -0700

    deps: use @eth-optimism/ganache-core

--- lib.js
+++ lib.js
@@ -1,6 +1,6 @@
 // make sourcemaps work!
 require('source-map-support').install();
 
-module.exports = require("ganache-core/public-exports.js");
-module.exports.version = require("ganache-core/package.json").version;
-module.exports.to = require("ganache-core/lib/utils/to");
+module.exports = require("@eth-optimism/ganache-core/public-exports.js");
+module.exports.version = require("@eth-optimism/ganache-core/package.json").version;
+module.exports.to = require("@eth-optimism/ganache-core/lib/utils/to");
